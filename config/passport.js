3dda7e8c970ffcfd6c24ae718642e68d7503a34f
JoeVincentt <eugene.bb@hotmail.com>
2018-11-28 21:51:22 -0500

    functionality  google oath20 added

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("./keys");
+
+module.exports = function(passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.clientSecret,
+        callbackURL: "/auth/google/callback",
+        proxy: true
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        // User.findOrCreate({ googleId: profile.id }, function(err, user) {
+        //   return cb(err, user);
+        // });
+        console.log(accessToken);
+        console.log(profile);
+      }
+    )
+  );
+};
