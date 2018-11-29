01b787466ee4698419be4f9e75b69f21d73cc5ae
JoeVincentt <eugene.bb@hotmail.com>
2018-11-29 00:23:02 -0500

    Mongoose schema added and users created in DB

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("./keys");
+//Load User model
+const User = mongoose.model("users");
 
 module.exports = function(passport) {
   passport.use(
@@ -12,11 +14,34 @@ module.exports = function(passport) {
         proxy: true
       },
       (accessToken, refreshToken, profile, done) => {
-        // User.findOrCreate({ googleId: profile.id }, function(err, user) {
-        //   return cb(err, user);
-        // });
-        console.log(accessToken);
-        console.log(profile);
+        // console.log(accessToken);
+        // console.log(profile);
+
+        const image = profile.photos[0].value.substring(
+          0,
+          profile.photos[0].value.indexOf("?")
+        );
+
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: image
+        };
+
+        //Check for existing User
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            //Return User
+            done(null, user);
+          } else {
+            //Create new user
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
       }
     )
   );
