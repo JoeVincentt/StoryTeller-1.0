1c76dfa3183813e4152954e4bee84c120bd6a78e
JoeVincentt <eugene.bb@hotmail.com>
2018-11-28 21:31:58 -0500

    add google auth credentials, add mongodb credentials

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb://eugene:123456a@ds223760.mlab.com:23760/storybooks-dev-eb",
+  googleClientID:
+    "115218689504-eeuoppfob3s9jf0ll68mdr5lr6vvcsfl.apps.googleusercontent.com",
+  googleClientSecret: "TWiWp33bi-mUug2ww4oC7Day"
+};
