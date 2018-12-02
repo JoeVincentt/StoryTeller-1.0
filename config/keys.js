6a42fda3e19e689d7e93bbace528eecd5cd571d7
JoeVincentt <eugene.bb@hotmail.com>
2018-12-02 13:17:31 -0500

    dev key prod keys last before deployment

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://eugene:123456a@ds223760.mlab.com:23760/storybooks-dev-eb",
-  googleClientID:
-    "115218689504-uuob96306uen437oms0ioe8ks26mhlst.apps.googleusercontent.com",
-  googleClientSecret: "Uho65ixy9xp7n4gobe8lwzlq"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
