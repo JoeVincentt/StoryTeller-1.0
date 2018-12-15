5fcf66316e484a242d093cd0a1f58b5d6f95230e
JoeVincentt <eugene.bb@hotmail.com>
2018-12-15 00:05:53 -0500

    little chanches in dashboard.handlebars

--- routes/stories.js
+++ routes/stories.js
@@ -119,6 +119,7 @@ router.put("/:id", (req, res) => {
     let allowComments;
 
     if (req.body.allowComments) {
+      console.log(req.body.allowComments);
       allowComments = true;
     } else {
       allowComments = false;
